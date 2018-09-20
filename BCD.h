97cba9704335b33dae6bdd825658792e4fe2c93c
alisson-camara <30715701+alisson-camara@users.noreply.github.com>
2018-09-20 10:59:38 -0300

    bcd.H

---
+++ BCD.h
@@ -0,0 +1,22 @@
+#ifndef BCD_H
+#define	BCD_H
+
+#include <xc.h>
+
+void BCD_init();
+
+void BCD_output(int saida);
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
